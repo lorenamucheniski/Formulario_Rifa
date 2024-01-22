5b5e01011deb6f53ebbe210d543314c580748909
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-21 21:56:40 -0300

    feat: add fetch em uma api localhost

---
+++ index.js
@@ -0,0 +1,58 @@
+const fomulario = document.querySelector("form")
+const botao = document.querySelector(".button")
+const iname = document.querySelector("#nome")
+const telefone = document.querySelector("#telefone")
+const numero = document.querySelector("#numeros")
+const valorPago = document.querySelector("#valor_pago")
+
+function cadastrar() {
+    fetch('http://localhost:8080/cliente',
+    {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            nome: iname.value,
+            numeroDeRifas: numero.value,
+            numeroDeTelefone: telefone.value,
+            total: valorPago.value
+        })
+    })
+    .then(
+        function (res) {
+            res.json() 
+        })
+    .then(data => console.log(data))
+    .catch(function (res) {console.log(res) })
+
+
+    
+}
+
+function ola() {
+    const uris = 'http://localhost:8080/rifa'
+    fetch(uris,
+    {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            id: uris.getId,
+            numeroDaRifa: numero.value
+        })
+    }).then(
+        function(res) {
+            res.json()
+        }
+    )
+}
+
+fomulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    cadastrar();
+    ola()
+})
